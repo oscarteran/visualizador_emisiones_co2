99e98739347b50bd4e9275a88b153a8336fd423e
oscarteran <oscar-hdzteran@hotmail.com>
2024-10-15 19:16:06 -0600

    Impresion de carpetas unicas

--- src/app.py
+++ src/app.py
@@ -20,10 +20,10 @@ only_files = [f for f in files_and_folders_in_directory if os.path.isfile(os.pat
 # Filtrar solo las carpetas
 only_folders = [f for f in files_and_folders_in_directory if os.path.isdir(os.path.join(current_directory, f))]
 
-# Mostrar la lista de archivos
-print("Archivos en el directorio actual:")
-for file in only_files:
-    print(file)
+# # Mostrar la lista de archivos
+# print("Archivos en el directorio actual:")
+# for file in only_files:
+#     print(file)
 
 # Mostrar la lista de carpetas
 print("\nCarpetas en el directorio actual:")
