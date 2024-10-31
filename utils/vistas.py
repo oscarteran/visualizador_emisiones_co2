be247c2ae3e93e8b8ce414f6dd9487a58c056e5a
oscarteran <oscar-hdzteran@hotmail.com>
2024-10-31 13:48:37 -0600

    Rutas de mapas

--- utils/vistas.py
+++ utils/vistas.py
@@ -107,6 +107,19 @@ def contenido_principal():
         unsafe_allow_html=True
     )
     
+    st.markdown(
+        """
+        <div style="text-align: justify;">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    
+    
+    
+    
     
 
 def listado_mapas():
